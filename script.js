70462f3f9a8dd2d4d71d83ec2dcdbcbb8a77bd3d
sergiogonza <sergiogonzawr@gmail.com>
2025-03-20 22:31:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ const material1 = new THREE.MeshStandardMaterial({
 // Load the model
 const objloader = new OBJLoader();
 objloader.load(
-    'https://cdn.glitch.global/c536c49d-7661-4fd9-81b8-918d12539028/dionysius.obj',
+    'https://cdn.glitch.global/c536c49d-7661-4fd9-81b8-918d12539028/dionysius2.obj',
  (object) => {
         object.children[0].material = material1;
         object.scale.setScalar( 20 );
